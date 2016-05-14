1bb8e70066c3a7304c63a94428475528d6f1b8e7
Leland Jordon <lelandjordon@mac.com>
2016-05-14 11:53:38 -0400

    Can display only the names of all restaurants.

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ mongo.connect(url, function(err, db){
   var number = prompt("Type 1 and press enter to display all restaurants' names: ")
   if(number == "1"){
     collection.find().toArray(function(err, doc){
-      console.log(doc.name);
+        console.log(doc.length);
+        for (var i=0; i<doc.length; ++i){
+            console.log(doc[i].name);
+        }
     })
   }
     else if(number == "2"){
@@ -18,3 +21,14 @@ mongo.connect(url, function(err, db){
     };
 
 });
+
+
+// var result = objArray.map(function(a) {return a.foo;});
+// function getFields(input, field) {
+//     var output = [];
+//     for (var i=0; i < input.length ; ++i)
+//         output.push(input[i][field]);
+//     return output;
+// }
+
+// var result = getFields(objArray, "foo"); // returns [ 1, 3, 5 ]
