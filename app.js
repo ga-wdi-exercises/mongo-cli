81c9d04c1d17912636b17497f264514ca0b2a0c5
Zaneh Williams <zanehmw@gmail.com>
2016-05-15 08:56:13 -0400

    attempted to do display info of restaurants when requested but I was returned too much info?

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var prompt = require("prompt-sync")()
 
 mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
-  var number = prompt("Type 1 and press enter to display all restaurants' names: ")
-  collection.find().forEach(function(u) {
-    var result = [];
-    result.push(u.name)
-    console.log(result)
-  })
+  var number = prompt("Type 1 and press enter to display all restaurants' names: " )
+  if(number == "1"){
+    collection.find().forEach(function(u) {
+      var result = [];
+      result.push(u.name)
+      console.log(result)
+    })
   }
-)
+})
+
+mongo.connect(url, function(err, db){
+  var collection = db.collection('restaurants');
+  var display = prompt("Type the name of a restaurant to view more info ")
+  if(display == "Cookies Corner"){
+    console.log(collection.find({name: "Cookies Corner"}))
+  }
+})
