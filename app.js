ff93a6bb4e4599d2e7c41d86211d017390434d6a
Zaneh Williams <zanehmw@gmail.com>
2016-05-15 01:51:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var mongo = require("mongodb").MongoClient
+var url = "mongodb://localhost:27017/restaurant_db"
+var prompt = require("prompt-sync")()
+
+mongo.connect(url, function(err, db){
+  var collection = db.collection('restaurants');
+  var number = prompt("Type 1 and press enter to display all restaurants' names: ")
+  if(number == "1"){
+    collection.find().toArray(function(err, doc){
+      console.log(doc)
+    })
+  }
+})
