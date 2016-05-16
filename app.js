b3748891caafa38a86d2fd5139bd9b2924eb5975
Wes Reed <wesley.a.reed@gmail.com>
2016-05-16 09:59:37 -0400

    Add else condition for name input

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var mongo = require("mongodb").MongoClient
-var prompt = require("prompt-sync")()
+var prompt = require('prompt-sync')();
 var url = "mongodb://localhost:27017/restaurant_db"
 
 
 mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
-  var number = prompt("Type 1 and press enter to display all restaurants' names: ")
-  if(number == "1"){
+  var input = prompt('Type 1 and press enter to display all names of restaurants. Type restaurant name and press enter to display a single restaurant: ');
+  if(input == "1"){
     collection.find().toArray(function(err, doc){
-      console.log(doc);
-    })
-  var name = prompt("Enter the name of a restaurant you would like to view: ")
-  if(name){
-    collection.find({name: name}).toArray(function(err, doc){
-      console.log(doc);
+      console.log(doc)
     })
   }
-  }
+  else{
+    collection.find({"name": input}).toArray(function(err, doc){
+      console.log(doc)
+    })
+  };
 });
