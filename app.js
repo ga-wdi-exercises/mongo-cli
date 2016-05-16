c1eb96fb567686ab18e3e22e857d9427750be3cc
Wes Reed <wesley.a.reed@gmail.com>
2016-05-15 22:41:52 -0400

    Allow users to find restaurants by name

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var mongo = require("mongodb").MongoClient
+var prompt = require("prompt-sync")()
 var url = "mongodb://localhost:27017/restaurant_db"
+
 
 mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
-  collection.find().toArray(function(err, docs){
-    console.log(docs);
-  })
-})
+  var number = prompt("Type 1 and press enter to display all restaurants' names: ")
+  if(number == "1"){
+    collection.find().toArray(function(err, doc){
+      console.log(doc);
+    })
+  var name = prompt("Enter the name of a restaurant you would like to view: ")
+  if(name){
+    collection.find({name: name}).toArray(function(err, doc){
+      console.log(doc);
+    })
+  }
+  }
+});
